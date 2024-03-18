56b62c8854b3d82fcaeee9a56f079e47ffa1cc4a
kraka123 <cool.krakasevich@mail.ru>
2024-03-18 17:16:42 +0300

    second

---
+++ main.php
@@ -0,0 +1,400 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <title>Unearth</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link href="https://fonts.googleapis.com/css?family=Rubik:300,400,700|Oswald:400,700" rel="stylesheet">
+
+    <link rel="stylesheet" href="fonts/icomoon/style.css">
+
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/jquery.fancybox.min.css">
+    <link rel="stylesheet" href="css/owl.carousel.min.css">
+    <link rel="stylesheet" href="css/owl.theme.default.min.css">
+    <link rel="stylesheet" href="fonts/flaticon/font/flaticon.css">
+    <link rel="stylesheet" href="css/aos.css">
+
+    <!-- MAIN CSS -->
+    <link rel="stylesheet" href="css/style.css">
+    
+  </head>
+  <body data-spy="scroll" data-target=".site-navbar-target" data-offset="300">
+
+  <div id="overlayer"></div>
+  <div class="loader">
+    <div class="spinner-border text-primary" role="status">
+      <span class="sr-only">Загрузка...</span>
+    </div>
+  </div>
+  
+  <div class="site-wrap"  id="home-section">
+
+    <div class="site-mobile-menu site-navbar-target">
+      <div class="site-mobile-menu-header">
+        <div class="site-mobile-menu-close mt-3">
+          <span class="icon-close2 js-menu-toggle"></span>
+        </div>
+      </div>
+      <div class="site-mobile-menu-body"></div>
+    </div>
+   
+
+      
+    <header class="site-navbar js-sticky-header site-navbar-target" role="banner">
+
+      <div class="container">
+        <div class="row align-items-center position-relative">
+          
+            
+            <div class="site-logo">
+              <a href="index.html" class="text-black"><span class="text-primary">Учет ПО</a>
+            </div>
+            
+            <div class="col-12">
+              <nav class="site-navigation text-right ml-auto " role="navigation">
+
+                <ul class="site-menu main-menu js-clone-nav ml-auto d-none d-lg-block">
+                <li><a href="#home-section" class="nav-link">Главная</a></li>
+                <li><a href="#services-section" class="nav-link">Услуги</a></li>
+              
+
+                <li class="has-children">
+                  <a href="#about-section" class="nav-link">О нас</a>
+                  <ul class="dropdown arrow-top">
+                    <li><a href="#team-section" class="nav-link">Сотрудники</a></li>
+                    <li><a href="#pricing-section" class="nav-link">Стоимость услуги предметной области</a></li>
+                    <li class="has-children">
+                    </li>
+                  </ul>
+                </li>
+                <li><a href="#contact-section" class="nav-link">Контакты</a></li>
+                <li><a href="index.php" class="nav-link">Выход</a></li>
+              </ul>
+              </nav>
+          
+            </div>
+
+          <div class="toggle-button d-inline-block d-lg-none"><a href="#" class="site-menu-toggle py-5 js-menu-toggle text-black"><span class="icon-menu h3"></span></a></div>
+
+        </div>
+      </div>
+      
+    </header>
+    
+    <div class="owl-carousel slide-one-item">
+      
+
+      
+
+      <div class="site-section-cover overlay img-bg-section" style="background-image: url('images/111.jpg'); " >
+        <div class="container">
+          <div class="row align-items-center justify-content-center text-center">
+            <div class="col-md-12 col-lg-7">
+              <h1 data-aos="fade-up" data-aos-delay=""> Учет предметного обеспечения</h1>  
+            </div>
+          </div>
+        </div>
+
+      </div>
+
+      <div class="site-section-cover overlay img-bg-section" style="background-image: url('images/111.jpg'); " >
+        <div class="container">
+          <div class="row align-items-center justify-content-center text-center">
+            <div class="col-md-12 col-lg-8">
+              <h1 data-aos="fade-up" data-aos-delay="">Примеры предметного обеспечения </h1>      
+              <p data-aos="fade-up" data-aos-delay="200"><a href="#" class="btn btn-outline-white border-w-2 btn-md">Смотреть</a></p>
+            </div>
+          </div>
+        </div>
+
+      </div>
+      
+
+    </div>
+    
+    <div class="site-section">
+      <div class="block__73694 mb-2" id="services-section">
+        <div class="container">
+          <div class="row d-flex no-gutters align-items-stretch">
+
+            <div class="col-12 col-lg-6 block__73422" style="background-image: url('images/222.jpg');" data-aos="fade-right" data-aos-delay="">
+            </div>
+
+            
+
+            <div class="col-lg-5 ml-auto p-lg-5 mt-4 mt-lg-0" data-aos="fade-left" data-aos-delay="">
+              <h2 class="mb-3 text-black">Услуги предметного обеспечения</h2>
+              <p>Услуги предметного обеспечения - это специализированные услуги, направленные на обеспечение клиентов необходимым оборудованием, инструментами или материалами для выполнения определенных задач или работ. Эти услуги могут включать в себя аренду, поставку, установку, обслуживание и ремонт предметов обеспечения.</p>
+
+              <p>Примеры услуг предметного обеспечения могут включать в себя:
+
+1. Аренда строительной техники (экскаваторы, бульдозеры и др.) для строительных работ.
+2. Поставка офисной техники (принтеры, сканеры, копировальные аппараты) для офисных нужд.
+3. Обслуживание медицинского оборудования (ультразвуковые аппараты, рентгеновские аппараты) в медицинских учреждениях.
+4. Поставка строительных материалов (кирпичи, песок, цемент) для строительных проектов.
+
+Услуги предметного обеспечения играют важную роль в различных отраслях экономики, обеспечивая компаниям доступ к необходимому оборудованию и материалам без необходимости приобретения их на постоянной основе</p>
+              
+            </div>
+
+          </div>
+        </div>      
+      </div>
+
+
+      <div class="block__73694">
+        <div class="container">
+          <div class="row d-flex no-gutters align-items-stretch">
+
+            <div class="col-12 col-lg-6 block__73422 order-lg-2" style="background-image: url('images/333.jpg');" data-aos="fade-left" data-aos-delay="">
+            </div>
+
+            
+
+            <div class="col-lg-5 mr-auto p-lg-5 mt-4 mt-lg-0 order-lg-1" data-aos="fade-right" data-aos-delay="">
+              <h2 class="mb-3 text-black">Одна из услуг предметной области это аренда строительной техники</h2>
+              <p>Аренда строительной техники - это услуга предметного обеспечения, которая позволяет компаниям и частным лицам временно использовать специализированную строительную технику без необходимости приобретения ее. Это удобный и экономически выгодный способ получить доступ к необходимому оборудованию для выполнения строительных работ.</p>
+
+              <p>Преимущества аренды строительной техники включают в себя:<br>
+1. Экономия затрат: нет необходимости покупать дорогостоящее оборудование, оплачивать его обслуживание и хранение.
+2. Гибкость: возможность арендовать различные виды техники в зависимости от конкретных задач и сроков выполнения работ.
+3. Техническая поддержка: обычно арендодатели предоставляют техническую поддержку и обслуживание арендованного оборудования.
+4. Удобство: аренда позволяет быстро получить необходимую технику без долгих процедур покупки.</p>
+
+            </div>
+
+          </div>
+        </div>      
+      </div>
+    </div>
+
+        </div>
+      </div>
+    </div>
+    <div class="site-section" id="team-section">
+      <div class="container">
+        <div class="row mb-5 justify-content-center">
+          <div class="col-md-7 text-center">
+            <div class="block-heading-1" data-aos="fade-up" data-aos-delay="">
+              <h2>Наша команда</h2>
+              <p>Наша команда – это опытные специалисты, хорошо разбирающиеся во всех задачах, которые ложатся на их плечи, и умеющие находить выход из любых, даже самых сложных ситуаций. </p>
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-lg-4 col-md-6 mb-4" data-aos="fade-up">
+            <div class="block-team-member-1 text-center rounded">
+              <figure>
+                <img src="images/555.jpg" alt="Image" class="img-fluid rounded-circle">
+              </figure>
+              <h3 class="font-size-20 text-black">Глеб Жиглов</h3>
+              <span class="d-block font-gray-5 letter-spacing-1 text-uppercase font-size-12 mb-3">Mining Expert</span>
+              <div class="block-social-1">
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6 mb-4" data-aos="fade-up" data-aos-delay="100">
+            <div class="block-team-member-1 text-center rounded">
+              <figure>
+                <img src="images/666.jpg" alt="Image" class="img-fluid rounded-circle">
+              </figure>
+              <h3 class="font-size-20 text-black">Володя Шарапов</h3>
+              <span class="d-block font-gray-5 letter-spacing-1 text-uppercase font-size-12 mb-3">Project Manager</span>
+              <div class="block-social-1">
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6 mb-4" data-aos="fade-up" data-aos-delay="200">
+            <div class="block-team-member-1 text-center rounded">
+              <figure>
+                <img src="images/777.jpg" alt="Image" class="img-fluid rounded-circle">
+              </figure>
+              <h3 class="font-size-20 text-black">Дмитрий Смирнов</h3>
+              <span class="d-block font-gray-5 letter-spacing-1 text-uppercase font-size-12 mb-3">Engineer</span>
+              <div class="block-social-1">
+                
+              </div>
+            </div>
+          </div>
+
+      </div>
+    </div>
+
+
+    <section class="site-section bg-light" id="pricing-section">
+      <div class="container">
+        <div class="row mb-5 justify-content-center text-center">
+          <div class="col-md-7">
+            <div class="block-heading-1" data-aos="fade-up" data-aos-delay="">
+              <h2>Цены на аренду</h2>
+            </div>
+          </div>
+        </div>
+        <div class="row mb-5">
+          <div class="col-md-6 mb-4 mb-lg-0 col-lg-4" data-aos="fade-up" data-aos-delay="">
+            <div class="pricing">
+              <h3 class="text-center text-black">Эконом</h3>
+              <div class="price text-center mb-4 ">
+                <span><span>20.000 рублей</span> / year</span>
+              </div>
+              <ul class="list-unstyled ul-check success mb-5">
+                
+                <li>Б/У техника</li>
+              </ul>
+              <p class="text-center">
+                <a href="#" class="btn btn-secondary btn-md">Оформить</a>
+              </p>
+            </div>
+          </div>
+
+          <div class="col-md-6 mb-4 mb-lg-0 col-lg-4" data-aos="fade-up" data-aos-delay="100">
+            <div class="pricing">
+              <h3 class="text-center text-black">Премиум</h3>
+              <div class="price text-center mb-4 ">
+                <span><span>50.000 руюлей</span> / year</span>
+              </div>
+              <ul class="list-unstyled ul-check success mb-5">
+                
+                <li>Поддержаная техника</li>
+              </ul>
+              <p class="text-center">
+                <a href="#" class="btn btn-primary btn-md text-white">Оформить</a>
+              </p>
+            </div>
+          </div>
+
+          <div class="col-md-6 mb-4 mb-lg-0 col-lg-4" data-aos="fade-up" data-aos-delay="200">
+            <div class="pricing">
+              <h3 class="text-center text-black">Люкс</h3>
+              <div class="price text-center mb-4 ">
+                <span><span>100.000 рублей</span> / year</span>
+              </div>
+              <ul class="list-unstyled ul-check success mb-5">
+                
+                <li>Новая техника</li>
+              </ul>
+              <p class="text-center">
+                <a href="#" class="btn btn-secondary btn-md">Оформить</a>
+              </p>
+            </div>
+          </div>
+        </div>
+    
+    </section>
+
+
+  
+
+
+    <div class="site-section bg-light" id="contact-section">
+      <div class="container">
+        <div class="row">
+          <div class="col-12 text-center mb-5" data-aos="fade-up" data-aos-delay="">
+            <div class="block-heading-1">
+              <h2>Контакты</h2>
+              <div class="wrapper">
+            <br>
+                <div class="ico-wrap"> <i class="fa fa-map-marker ico-contact"></i>
+                </div>
+                <div class="descript-wrapper">
+                    <h4 class="aio-icon-title">Наш фактический адрес:</h4>
+                    <div class="aio-icon-description">
+                       г. Ярославль, ул.Нахимсона д.5</div>
+                </div>
+            
+                <span class="clearfix"></span>
+            
+                <div class="ico-wrap"> <i class="fa fa-clock-o ico-contact"></i>
+                </div>
+                <div class="descript-wrapper">
+                    <h4 class="aio-icon-title">Время работы офиса:</h4>
+                    <div class="aio-icon-description">
+                        пн-вс с 8:00 до 20:00, без обеда и выходных.</div>
+                </div>
+            
+                <span class="clearfix"></span>
+            
+                <div class="ico-wrap">
+                    <i class="fa fa-envelope ico-contact"></i>
+                </div>
+                <div class="descript-wrapper">
+                    <h4 class="aio-icon-title">Электронная почта:</h4>
+                    <div class="aio-icon-description">
+                        stroitel.yar@mail.ru</div>
+                </div>
+            
+                <span class="clearfix"></span>
+            
+                <div class="ico-wrap">
+                    <i class="fa fa-phone ico-contact"></i>
+                </div>
+                <div class="descript-wrapper">
+                    <h4 class="aio-icon-title">Номер для связи:</h4>
+                    <div class="aio-icon-description">
+                        8 (999) 777-66-66</div>
+                </div>
+           
+                <span class="clearfix"></span>
+             <br>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+
+    <footer class="site-footer">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-6">
+            <div class="row">
+              <div class="col-md-8">
+                <h2 class="footer-heading mb-4">UnEarth</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque facere laudantium magnam voluptatum autem. Amet aliquid nesciunt veritatis aliquam.</p>
+              </div>
+              <div class="col-md-4 ml-auto">
+                <h2 class="footer-heading mb-4">Компания</h2>
+                <ul class="list-unstyled">
+                  <li><a href="#home-section" class="nav-link">Главная</a></li>
+                  <li><a href="#services-section" class="nav-link">Услуги</a></li>
+                  <li><a href="#about-section" class="nav-link">О нас</a></li>
+                  <li><a href="#team-section" class="nav-link">Команда</a></li>
+                  <li><a href="#pricing-section" class="nav-link">Цены</a></li>
+                  <li><a href="#contact-section" class="nav-link">Контакты</a></li>
+                </ul>
+              </div>
+
+
+        <div class="row pt-5 mt-5 text-center">
+          <div class="col-md-12">
+            <div class="border-top pt-5">
+            <p class="copyright"><small>
+            <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+            Copyright &copy;<script>document.write(new Date().getFullYear());</script> by Rossiiskaya Kseniya </a>
+            <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+            </small></p>
+            </div>
+
+    </footer>
+
+  </div>
+
+  <script src="js/jquery-3.3.1.min.js"></script>
+  <script src="js/popper.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <script src="js/owl.carousel.min.js"></script>
+  <script src="js/jquery.sticky.js"></script>
+  <script src="js/jquery.waypoints.min.js"></script>
+  <script src="js/jquery.animateNumber.min.js"></script>
+  <script src="js/jquery.fancybox.min.js"></script>
+  <script src="js/jquery.easing.1.3.js"></script>
+  <script src="js/aos.js"></script>
+  
+  <script src="js/main.js"></script>
+    
+  </body>
+</html>
